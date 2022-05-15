a774fbecb09055344d32901cc378657bb77272aa
Narmin <mnermin064@gmail.com>
2022-05-15 19:57:28 +0400

    new sections added

--- project-1/src/index.js
+++ project-1/src/index.js
@@ -23,7 +23,7 @@ class Section1 extends Component{
                  <li><a href="">Blog</a></li>
                  <li><a href="">Faq</a></li>
                  <li><a href="">Contact Us</a></li>
-                <button>Book Now </button>
+                <button><i class="fa-solid fa-plane"></i>Book Now </button>
                </ul>
              </div>
            </div>
@@ -48,12 +48,75 @@ class Section1 extends Component{
        </div>
      </div>
      </section>
-    
+    )
+  }
+}
+
+class Section2 extends Component{
+  render(){
+    return(
+      <section className='second'>
+<div className="container h-100">
+  <div className="row h-100">
+   <div className="col-lg-6 h-100">
+     <div className="left-img">
+       <div className="left-txt">
+         <h1>
+Business Jet Charter</h1>
+<p>
+Trade crowded airports and wasted time for the ease, comfort, and convenience of travel by private jet.</p>
+<a href=""><i class="fa-solid fa-plane"></i>Book Now</a>
+       </div>
+     </div>
+   </div>
+   <div className="col-lg-6 h-100">
+     <div className="right-img">
+     <div className="right-txt">
+     <h5><span>#1</span> Private Jet Charter</h5>
+       <ul>
+         <li>01 Private Jet Charter</li>
+         <li>Business Jet Charter</li>
+         <li>03 Private Helicopter</li>
+         <li>04 Air Ambulance</li>
+       </ul>
+
+     </div>
+     </div>
+   </div>
+  </div>
+</div>
+      </section>
     )
   }
 }
 
 
+class Section3 extends Component{
+  render(){
+    return(
+      <section className="third">
+   <img src="img/img.png" className='img'/>
+   <div className="container h-100">
+     <div className="row h-100" >
+       <div className="col-lg-7 h-100">
+         <div className="txt">
+           <h4><span>About</span> Flynext</h4>
+           <h1>Private Jet Charters save your time and give more comfort</h1>
+           <p>Flynext is the only way to fully travel on your terms. Whether it’s accessing a remote destination or taking back control of productivity and flight scheduling</p>
+           <div className="card-bottom">
+             <h2>Call for book an order</h2>
+             <h3>8-800-10-500
+</h3>
+<button><i class="fa-solid fa-plane"></i>Book Now</button>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+      </section>
+    )
+  }
+}
 
 
  class App extends Component {
@@ -61,6 +124,8 @@ class Section1 extends Component{
     return (
       <>
       <Section1/>
+      <Section2/>
+      <Section3/>
       </>
     )
   }
