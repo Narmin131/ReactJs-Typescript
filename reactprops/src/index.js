ba1378ee4d9a75793d76bb10a6437fcfa0627bfc
Narmin <mnermin064@gmail.com>
2022-05-20 21:41:15 +0400

    components file added

---
+++ reactprops/src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import  ReactDOM  from 'react-dom';
+import "./assets/sass/style.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
