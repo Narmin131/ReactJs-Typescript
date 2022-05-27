1391dcb1ac2f1ac2b0fe24a40a8f57cf89c45fcf
Narmin <mnermin064@gmail.com>
2022-05-27 23:46:19 +0400

    Github finder app

---
+++ githubfinder/src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from "react-dom";
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      
+      </>
+    )
+  }
+}
+
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
