b2a480e48b44d1677bd0fc9a938ec9d404ef965f
Narmin <mnermin064@gmail.com>
2022-07-16 09:50:43 +0400

    typescript

---
+++ Typescript/lesson1-enter/main.js
@@ -0,0 +1,45 @@
+// let infoOne : number;
+// let infoTwo : string;
+// let infoThree : boolean;
+// infoOne = 7;
+// infoTwo = "salam";
+// infoThree = true;
+// console.log(infoOne);
+// console.log(infoTwo);
+// console.log(infoThree);
+// let infoArr : Array<number> = [1,4,3,4,5];
+// let infoArr : Array<number> 
+// infoArr = [1,2,3,4,5];
+// console.log(...infoArr);
+// let infoArr : number[];
+// infoArr = [1,2,3,4];
+// console.log(...infoArr);
+// ? -> optional deyerin verilmeyeceyi halda verilir
+// viod istifade olunan funksiyada return etmek olmaz
+// function infoFunc(val:string,val1?:number):void {
+//      return val + val1;
+// }
+// console.log(infoFunc("Hello"));
+// const infoArr =():void=>  {
+//      'Hello';
+// }
+// class Person{
+//     public iname:string;
+//     public isurname:string;
+//     public iage:number;
+//     constructor(name:string,surname:string,age:number){
+//         this.iname = name;
+//         this.isurname = surname;
+//         this.iage = age;
+//     }
+// }
+// class Employee extends Person{
+//     isalary:number;
+//     constructor(name:string,surname:string,phone:number,salary:number){
+//         super(name,surname,phone);
+//         this.isalary = salary;
+//     }
+// }
+// const infoPerson = new Person("Nermin ","Selcan",31);
+// const infoEmployee = new Employee("Ilahe","56",332,3454);
+// console.log(infoPerson.iname+infoPerson.isurname);
