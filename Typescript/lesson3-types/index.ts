c963a3ac7698824154accf21ab9b886d6ab451e3
Narmin <mnermin064@gmail.com>
2022-07-30 23:17:19 +0400

    types

---
+++ Typescript/lesson3-types/index.ts
@@ -0,0 +1,72 @@
+// let infoA = 8;
+// let info:number = 5;
+// console.log(info);
+
+
+
+// function a(data:string){
+//     console.log(data);
+//  }
+// a("Hello");
+
+// let infoObj:{
+//     name:string,
+//     surname:string,
+//     age:number,
+//     education:boolean,
+//     drive:any
+// }
+
+//  infoObj={
+//     name:"Narmin",
+//     surname:"Mammadova",
+//     age:18,
+//     education:true,
+//     drive:(carname:string)=>{
+//         return `${infoObj.name} + ${carname}`
+//     }
+// }
+
+// infoObj={
+//     name:"Narmin",
+//     surname:"Mammadova",
+//     age:18,
+//     education:true,
+//     drive:function(carname:string){
+//         return `${infoObj.name} + ${carname}`
+   
+//     }
+// }
+
+// infoObj={
+//     name:"Narmin",
+//     surname:"Mammadova",
+//     age:18,
+//     education:true,
+//     drive(carname:string){
+//         return `${infoObj.name} + ${carname}`
+   
+//     }
+// }
+
+
+// console.log(infoObj.name);
+// console.log(infoObj.drive("Nissan"));
+
+
+// let car:string = "red";
+
+// let colorCar = function(color:"blue" | "green" | 'red'):void{
+//         car = color;
+// }
+
+// colorCar("blue")
+// console.log(car);
+
+// let info : "html" | "css" | "js";
+// info="js";
+
+
+
+// let a : any = 5;
+// console.log(a);
